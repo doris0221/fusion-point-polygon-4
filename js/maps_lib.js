1f54dd87867fdc37bb97ff3a5e3e3dd05b39a044
doris0221 <yunzhuo.zhang@trincoll.edu>
2016-04-19 12:49:13 -0400

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -143,7 +143,6 @@ var MapsLib = {
     if ( $("#cbType2").is(':checked')) searchType += "2,";
     if ( $("#cbType3").is(':checked')) searchType += "3,";
     if ( $("#cbType4").is(':checked')) searchType += "4,";
-    if ( $("#cbType5").is(':checked')) searchType += "5,";
     whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";
 
 
